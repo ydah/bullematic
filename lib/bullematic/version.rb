a8240e6a79ddb03b2c09722e560e0f7f63ab093b
ydah <t.yudai92@gmail.com>
2026-01-13 12:08:46 +0900

    Initial commit

---
+++ lib/bullematic/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Bullematic
+  VERSION = "0.1.0"
+end
---
+++ spec/bullematic_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Bullematic do
+  it "has a version number" do
+    expect(Bullematic::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
